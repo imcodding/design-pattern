59b6d36e1a299689e9cbba3f3947406892d6fa85
sooyun <suy0226@naver.com>
2022-08-14 22:57:01 +0900

    Template 패턴

---
+++ src/main/java/com/design/template/MapView.java
@@ -0,0 +1,14 @@
+package com.design.template;
+
+public abstract class MapView {
+
+    protected abstract void connectMapServer();
+    protected abstract void showMapOnScreen();
+    protected abstract void moveToCurrentLocation();
+
+    public void initMap() {
+        connectMapServer();
+        showMapOnScreen();
+        moveToCurrentLocation();
+    }
+}
---
+++ src/main/java/com/design/template/TemplateDesign.java
@@ -0,0 +1,9 @@
+package com.design.template;
+
+public class TemplateDesign {
+
+    public TemplateDesign() {
+        new NaverMapView().initMap();
+        new KakaoMapView().initMap();
+    }
+}
